69fe733062b3a40e731878100604366f742a51c9
Dale Hanson <50219787+dalehanson@users.noreply.github.com>
2020-05-09 09:49:39 -0500

    Create Enviroment_Setup_Create_All_Empty_Customer_Database_Tables_in_Snowflake.py
    Replicates database environment structure in Snowflake from Mysql (copies DDL and recreates physical tables in snowflake)

---
+++ Enviroment_Setup_Create_All_Empty_Customer_Database_Tables_in_Snowflake.py
@@ -0,0 +1,99 @@
+from airflow import DAG
+from airflow.operators import DummyOperator, PythonOperator
+from airflow.hooks.base_hook import BaseHook
+from airflow.models import Variable
+from datetime import datetime, timedelta
+import Migration_All_Databases_Mysql_to_Snowflake_functions as functs
+
+##################################################################
+#Setting variable definitions and connections
+##################################################################
+
+parent_dag_name = 'Enviroment_Setup_Create_All_Empty_Customer_Database_Tables_in_Snowflake'
+
+Airflow_snowflake_connection_name = Variable.get('Airflow_snowflake_connection_name')
+Airflow_mysql_connection_name = Variable.get('Airflow_mysql_connection_name')
+orchestration_country = Variable.get('orchestration_country')
+
+database_include_patterns = ['prefix*'] #only inlcude databases that start with 'prefix', for multiple format as a list seperated by commas
+
+excluded_databases = Variable.get('excluded_databases')
+
+
+##################################################################
+#Collection Connection attributes from Airflow connections repo
+##################################################################
+
+
+sf_con = BaseHook.get_connection(Airflow_snowflake_connection_name)
+snowflake_username = sf_con.login 
+snowflake_password = sf_con.password 
+snowflake_account = sf_con.host 
+snowflake_database = 'A_UTILITY'
+snowflake_warehouse = "XSMALL" 
+
+
+
+mysql_con = BaseHook.get_connection(Airflow_mysql_connection_name)
+mysql_username = mysql_con.login 
+mysql_password = mysql_con.password 
+mysql_hostname = mysql_con.host
+mysql_port = mysql_con.port
+
+
+
+
+
+#############################################################################
+#Defining Dag structure
+#############################################################################
+    
+default_args = {
+    'owner': 'dale',
+    #'start_date': datetime.now(),
+    'start_date': datetime(2019, 9, 1),
+    'retry_delay': timedelta(minutes=.5)
+    
+}
+
+one_dag =  DAG(parent_dag_name, default_args=default_args, schedule_interval='@once') 
+
+database_list =  functs.get_database_list(trim_by_patterns = database_include_patterns, excluded_databases = excluded_databases)
+
+#start dummy task
+start = DummyOperator(
+    task_id='start',
+    dag=one_dag)
+
+
+# Creates the tasks dynamically.  Each one will elaborate one chunk of data.
+create_tables_for_multiple_dbs = PythonOperator(
+        #provide_context=True,
+        task_id='create_tables_for_multiple_dbs',
+        pool='Pool_max_parallel_5',
+        python_callable=functs.generate_sf_multiple_db_tables,
+        op_kwargs={
+        'db_list': database_list,
+        'mysql_username': mysql_username,
+        'mysql_password': mysql_password,
+        'mysql_hostname': mysql_hostname,
+        'mysql_port': mysql_port,
+        'snowflake_username': snowflake_username,
+        'snowflake_password': snowflake_password,
+        'snowflake_account': snowflake_account,
+        'snowflake_database': snowflake_database,
+        },
+        dag=one_dag)
+
+    
+#end dummy dag
+end = DummyOperator(
+    task_id='end',
+    dag=one_dag)
+
+
+
+
+#Setting dependencies, the configuration below creates a parallel task for each table  that migrates the table from mysql to s3, then from s3 to 
+start  >> create_tables_for_multiple_dbs
+create_tables_for_multiple_dbs >> end
